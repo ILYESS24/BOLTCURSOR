6198ac34489f482b01d81c4ba3eb521d542fde9d
ILYESS24 <ilyessbenina3@gmail.com>
2025-11-26 21:08:59 +0100

    Remove 'use client' directives causing sourcemap errors
    - Removed 'use client'; from all .client.tsx components
    - In Remix, client components are identified by .client.tsx extension
    - This directive was causing sourcemap resolution errors during build
    - Components: NavBar.client.tsx, Typewriter.client.tsx, AnimatedBorder.client.tsx, BorderBeam.client.tsx

--- app/components/ui/BorderBeam.client.tsx
+++ app/components/ui/BorderBeam.client.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, { CSSProperties, useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import { classNames } from '~/utils/classNames';
